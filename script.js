83fa13e59426a7c6b29dfe4edd38265e0c2e5f42
NeuralGeniusAI <afara@neuralgenius.ai>
2025-01-06 17:17:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,186 @@
+function toggleMenu() {
+  var navbar = document.querySelector(".ul-mobile");
+  if (navbar.style.display === "flex") {
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "flex";
+  }
+}
+
+gsap.to(".specialization-areas .container-items", {
+  scrollTrigger: ".specialization-areas .container-items",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-automated-solutions .container .item-1", {
+  scrollTrigger: ".section-automated-solutions .container .item-1",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+
+gsap.to(".section-automated-solutions .container .item-2", {
+  scrollTrigger: ".section-automated-solutions .container .item-2",
+  duration: 1,
+  delay: 0.4,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-automated-solutions .container .item-3", {
+  scrollTrigger: ".section-automated-solutions .container .item-3",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-automated-solutions .container .item-4", {
+  scrollTrigger: ".section-automated-solutions .container .item-4",
+  duration: 1,
+  delay: 0.4,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-automated-solutions .container .item-5", {
+  scrollTrigger: ".section-automated-solutions .container .item-5",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+
+
+
+
+
+gsap.to(".section-neural-operations .container .container-options .item-1", {
+  scrollTrigger: ".section-neural-operations .container .container-options .item-1",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-neural-operations .container .container-options .item-2", {
+  scrollTrigger: ".section-neural-operations .container .container-options .item-2",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-neural-operations .container .container-options .item-3", {
+  scrollTrigger: ".section-neural-operations .container .container-options .item-3",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-neural-operations .container .container-options .item-4", {
+  scrollTrigger: ".section-neural-operations .container .container-options .item-4",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+
+
+
+
+
+
+gsap.to(".section-neural-marketing .container-marketing-services .item-1", {
+  scrollTrigger: ".section-neural-marketing .container-marketing-services .item-1",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+gsap.to(".section-neural-marketing .container-marketing-services .item-2", {
+  scrollTrigger: ".section-neural-marketing .container-marketing-services .item-2",
+  duration: 1.1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+gsap.to(".section-neural-marketing .container-marketing-services .item-3", {
+  scrollTrigger: ".section-neural-marketing .container-marketing-services .item-3",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+gsap.to(".section-neural-marketing .container-marketing-services .item-4", {
+  scrollTrigger: ".section-neural-marketing .container-marketing-services .item-4",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+gsap.to(".section-neural-marketing .container-marketing-services .item-5", {
+  scrollTrigger: ".section-neural-marketing .container-marketing-services .item-5",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+
+
+
+
+
+
+gsap.to(".section-user-feedback .container .item-1", {
+  scrollTrigger: ".section-user-feedback .container .item-1",
+  duration: 1,
+  delay: 0.3,
+  y: -150,
+  opacity: 1,
+});
+gsap.to(".section-user-feedback .container .item-2", {
+  scrollTrigger: ".section-user-feedback .container .item-2",
+  duration: 1.1,
+  delay: 0.4,
+  y: -150,
+  opacity: 1,
+});
+
+
+
+gsap.to(".section-home .container-text h1", {
+  duration: 1.1,
+  x: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-home .container-text p", {
+  duration: 1.1,
+  delay: 0.1,
+  x: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-home .container-text button", {
+  duration: 1.1,
+  delay: 0.2,
+  x: -150,
+  opacity: 1,
+});
+
+gsap.to(".section-home .container-images img", {
+  duration: 1.3,
+  delay: 0.1,
+  x: -150,
+  opacity: 1,
+});
+
